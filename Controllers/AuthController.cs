51f864f34d52fdad8c23672b48bc41d6a04ce677
Tomas <tomkasper01@gmail.com>
2024-08-21 11:47:09 +0300

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagementWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace HospitalManagementWebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login(UserLogin userLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                // Auth the user
+                return RedirectToAction("Index", "Home");
+            }
+            return View(userLogin);
+
+        }
+
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Register the user
+                return RedirectToAction("Index", "Home");
+            }
+            return View(user);
+        }
+    }
+}
